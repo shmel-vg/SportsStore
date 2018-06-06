ac47198213bc8f1267be43248c33ccff31596254
Shmigel Vladimir <shmel-vg@rambler.ru>
2018-06-06 09:29:10 +0300

    commit 4

---
+++ SportsStore.WebUI/Models/ProductsListViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using SportsStore.Domain.Entities;
+
+namespace SportsStore.WebUI.Models
+{   // Обертка для отправки с контроллера на представление в одном классе модели представления
+    public class ProductsListViewModel
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+    }
+}
