c9d754cc1cafbfd57d572598ee74069ed3d23172
Shmigel Vladimir <shmel-vg@rambler.ru>
2018-06-11 21:50:14 +0300

    commit 8

--- SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
+++ SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -7,6 +7,7 @@ using Ninject;
 using SportsStore.Domain.Abstract;
 using SportsStore.Domain.Concrete;
 using SportsStore.Domain.Entities;
+using System.Configuration;
 
 namespace SportsStore.WebUI.Infrastructure
 {
@@ -18,17 +19,28 @@ namespace SportsStore.WebUI.Infrastructure
             kernel = kernelParam;
             AddBindings();
         }
+
         public object GetService(Type serviceType)
         {
             return kernel.TryGet(serviceType);
         }
+
         public IEnumerable<object> GetServices(Type serviceType)
         {
             return kernel.GetAll(serviceType);
         }
+
         private void AddBindings()
         {
             kernel.Bind<IProductRepository>().To<EFProductRepository>();
+
+            EmailSettings emailSettings = new EmailSettings
+            {
+                WriteAsFile = bool.Parse(ConfigurationManager
+                    .AppSettings["Email.WriteAsFile"] ?? "false")
+            };
+            kernel.Bind<IOrderProcessor>().To<EmailOrderProcessor>()
+                .WithConstructorArgument("settings", emailSettings);
         }
     }
 }
